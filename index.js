fda636ff7279b65c5812059b24cb05cbd0c5905f
Ruth Jepkalal <kipchauijepkaa@gmail.com>
2022-08-19 13:35:29 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+
+    return customerName;
+    
+    }
+    var setBestCustomer;
+    function setBestCustomer(){
+        return bestCustomer = `not bob`;
+    }
+
+    function overwriteBestCustomer(parameter=`maybe bob`){
+        return bestCustomer = `maybe bob`;
+    }
+
+    function changeLeastFavoriteCustomer(){
+        const changeLeastFavoriteCustomer = "vane";
+
+        changeLeastFavoriteCustomer = "Jane";
+
+        return changeLeastFavoriteCustomer;
+    }
+
+
